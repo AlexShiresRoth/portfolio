d175734aa6186c0f6329e5f84c3df175d6eef5c4
Alex Roth <alexroth96@gmail.com>
2019-06-17 23:47:37 -0400

    style projects section sortof and add imgs

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -8,40 +8,75 @@ export default class Projects extends Component {
     super(props);
 
     this.state = {
-      projects: [
-        {
-          title: "Italiano Bros Website",
-          tech: "ReactJS, Express, Javascript, NodeJS",
-          desc:
-            "Simple dynamic and responsive website for a construction company.",
-          img: "img1.jpg"
-        },
-        {
-          title: "Send Noods",
-          tech: "ReactJS, Express, Javascript, NodeJS",
-          desc: `Dynamic and responsive web app for finding ramen restaurants around user location. 
-            Integrated with Google Places/Maps API.`,
-          img: "img1.jpg"
-        },
-        {
-          title: "Timer App",
-          tech: "PassportJS, Express, Javascript, NodeJS",
-          desc: `Simple timer app made with vanilla js and passportjs for user authentication.`,
-          img: "img1.jpg"
-        },
-        {
-          title: "Rothenberg Law Offices",
-          tech: "Express, Javascript, NodeJS",
-          desc: `Responsive and dynamic website for a law office.`,
-          img: "img1.jpg"
-        }
-      ]
+      projects: [],
     };
   }
 
+  apps = [
+    {
+      title: "Italiano Bros Website",
+      tech: "ReactJS, Express, Javascript, NodeJS",
+      desc: "Simple dynamic and responsive website for a construction company.",
+      img: ["italiano-bros.png"]
+    },
+    {
+      title: "Send Noods",
+      tech: "ReactJS, Express, Javascript, NodeJS",
+      desc: `Dynamic and responsive web app for finding ramen restaurants around user location. 
+        Integrated with Google Places/Maps API.`,
+      img: ["send-noods.png"]
+    },
+    {
+      title: "Timer App",
+      tech: "PassportJS, Express, Javascript, NodeJS",
+      desc: `Simple timer app made with vanilla js and passportjs for user authentication.`,
+      img: ["timerapp.png"]
+    },
+    {
+      title: "Rothenberg Law Offices",
+      tech: "Express, Javascript, NodeJS",
+      desc: `Responsive and dynamic website for a law office.`,
+      img: ["lawoffice.png"]
+    }
+  ];
+
+  games = [
+    {
+      title: "Room Escape",
+      tech: "Unreal Engine 4, C++",
+      desc:
+        "Escape from rooms by finding hidden triggers, before time runs out.",
+      img: ["img1.jpg"]
+    },
+    {
+      title: "Battlgrounds",
+      tech: "Unreal Engine 4, C++",
+      desc: "Fight against enemy vehicles on an alien terrain.",
+      img: ["img1.jpg"]
+    }
+  ];
+
+  toggleGames() {
+    if(this.state.projects === this.apps){
+        return this.setState({projects: this.games})
+    }
+  }
+  toggleApps(){
+      if(this.state.projects === this.games){
+          return this.setState({projects: this.apps})
+      }
+  }
+
+  componentDidMount(){
+      this.setState({ projects: this.apps })
+  }
   render() {
     return (
       <section className="section__projects">
+        <div className="section__projects--buttons">
+          <a onClick={() => this.toggleApps()}>WebApps</a>
+          <a onClick={() => this.toggleGames()}>Games</a>
+        </div>
         <ProjectsMap projects={this.state.projects} />
       </section>
     );
--- client/components/projects/ProjectsMap.js
+++ client/components/projects/ProjectsMap.js
@@ -6,6 +6,13 @@ export const ProjectsMap = props => {
   const renderProjects = props.projects.map(project => {
     return (
       <div className="section__projects--item">
+        <div className="section__projects--item--image-container">
+          { project.img.map((img,i) => {
+            return <figure className="section__projects--item--image-container--image">
+                  <img src={require(`../../../images/projects/${img}`)} key={`${i}`} alt={`${project.title}`}/>
+            </figure>
+          })}
+        </div>
         <div className="section__projects--item--title">
           <h2>{project.title}</h2>
         </div>
