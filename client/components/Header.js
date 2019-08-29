b545ca1d518a01f8e3656a3bfc03fff5951df7bd
Alex Roth <alexroth96@gmail.com>
2019-08-28 23:32:30 -0400

    rearrange header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import '../css/main.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../css/main.css";
 
-import { Icons } from './reusable/Icons';
+import { Icons } from "./reusable/Icons";
 
 export default class Header extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			urls: [
-				{
-					icon: <i className="fab fa-github" />,
-					url: `https://github.com/AlexShiresRoth?tab=repositories`,
-				},
-				{
-					icon: <i className="fab fa-codepen" />,
-					url: `https://codepen.io/Alexroth96/#`,
-				},
-				{
-					icon: <i className="fab fa-twitter" />,
-					url: `https://twitter.com/AlexShiresRoth`,
-				},
-			],
-		};
-	}
+    this.state = {
+      urls: [
+        {
+          icon: <i className="fab fa-github" />,
+          url: `https://github.com/AlexShiresRoth?tab=repositories`
+        },
+        {
+          icon: <i className="fab fa-codepen" />,
+          url: `https://codepen.io/Alexroth96/#`
+        },
+        {
+          icon: <i className="fab fa-twitter" />,
+          url: `https://twitter.com/AlexShiresRoth`
+        }
+      ]
+    };
+  }
 
-	render() {
-		return (
-			<section className="section__header">
-				<div className="section__header--overlay" />
-				<div className="section__header--logo">
-					<img src={require('../../images/logotwo.png')} alt="asr logo" />
-				</div>
-				<div className="section__header--title-and-desc">
-					<h1>Alex Shires Roth</h1>
-					<hr />
-					<p>Self Taught Software Developer</p>
-					<Icons icons={this.state.urls} />
-				</div>
-				<div className="section__header--buttons">
-					<Link to="/resume">
-						<button>Resume</button>
-					</Link>
-				</div>
-			</section>
-		);
-	}
+  render() {
+    return (
+      <section className="section__header">
+        <div className="section__header--overlay" />
+        <div className="section__header--title-and-desc">
+          <div className="section__header--logo">
+            <img src={require("../../images/logothree.png")} alt="asr logo" />
+          </div>
+          <h1>Alex Shires Roth</h1>
+          <hr />
+          <p>Self Taught Software Developer</p>
+          <Icons icons={this.state.urls} />
+        </div>
+        <div className="section__header--buttons">
+          <Link to="/resume">
+            <button>Resume</button>
+          </Link>
+        </div>
+      </section>
+    );
+  }
 }
