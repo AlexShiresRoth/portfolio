a62e6bd7c140a24361811f863332f57cf36f3253
Alex Rothenberg <alexroth96@gmail.com>
2019-10-10 22:56:53 -0400

    change background to wavy svg

--- client/components/Header.js
+++ client/components/Header.js
@@ -34,7 +34,9 @@ const Header = _ => {
   }
   return (
     <section className="section__header">
-      <div className="section__header--overlay" />
+      <div className="section__header--overlay">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ff4500" fill-opacity="1" d="M0,96L60,80C120,64,240,32,360,64C480,96,600,192,720,224C840,256,960,224,1080,213.3C1200,203,1320,213,1380,218.7L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+      </div>
       <div></div>
       <div className="section__header--title-and-desc">
         <div className="section__header--logo">
